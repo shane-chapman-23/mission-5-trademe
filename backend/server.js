b7509e05f81784958dc4f271ede7d4c724623181
shane-chapman-23 <chapmanshane23@gmail.com>
2025-09-18 16:00:02 +1200

    initial commit. project skeleton setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
---
+++ frontend/src/sharedComponents/auctionCards.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function auctionCards() {
+  return <div></div>;
+}
