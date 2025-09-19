5e1c74962d309e0f9abf6c3a649b9adb9ef4a809
Kenneth Sta Maria <kstamaria29@gmail.com>
2025-09-19 18:59:59 +1200

    style: fixed width and margins of navbar container

--- frontend/src/sharedComponents/navBar.jsx
+++ frontend/src/sharedComponents/navBar.jsx
@@ -10,9 +10,9 @@ function Navbar() {
   return (
     <>
       <header>
-        <nav className="bg-[#f6f5f4] h-[81px] border-b-1 border-[#e8e6e3]">
-          <div className="mr-auto ml-auto px-[140px] h-full flex items-center justify-center">
-            <div className="flex    ">
+        <nav className="bg-[#f6f5f4] h-[81px] border-b-1 border-[#e8e6e3] ">
+          <div className="mr-auto ml-auto px-[140px] max-w-[1600px] h-full flex items-center justify-between">
+            <div className="flex  w-full  ">
               <div className="mr-[8px] flex items-center">
                 <img src={logo} alt="Trademe Logo" className="w-[190px] h-[25px]" />
               </div>
@@ -25,27 +25,27 @@ function Navbar() {
               <ul className="flex">
                 <li className="px-[12px] h-[42px] flex flex-col items-center justify-center ">
                   <img src={categories} className="h-[16px] w-[16px]" />
-                  <span className="text-[14px]">Categories</span>
+                  <span className="text-[14px] whitespace-nowrap text-[#2f2c28]">Categories</span>
                 </li>
                 <li className="px-[12px] h-[42px] flex flex-col items-center justify-center ">
                   <img src={notifications} className="h-[16px] w-[16px]" />
-                  <span className="text-[14px]">Notifications</span>
+                  <span className="text-[14px] whitespace-nowrap">Notifications</span>
                 </li>
                 <li className="px-[12px] h-[42px] flex flex-col items-center justify-center ">
                   <img src={watchList} className="h-[16px] w-[16px]" />
-                  <span className="text-[14px]">Watchlist</span>
+                  <span className="text-[14px]  whitespace-nowrap">Watchlist</span>
                 </li>
                 <li className="px-[12px] h-[42px] flex flex-col items-center justify-center ">
                   <img src={favourites} className="h-[16px] w-[16px]" />
-                  <span className="text-[14px]">Favourites</span>
+                  <span className="text-[14px] whitespace-nowrap">Favourites</span>
                 </li>
                 <li className="px-[12px] h-[42px] flex flex-col items-center justify-center ">
                   <img src={startListing} className="h-[16px] w-[16px]" />
-                  <span className="text-[14px]">Start a listing</span>
+                  <span className="text-[14px] whitespace-nowrap">Start a listing</span>
                 </li>
                 <li className="px-[12px] h-[42px] flex flex-col items-center justify-center ">
                   <img src={categories} className="h-[16px] w-[16px]" />
-                  <span className="text-[14px]">My Trade Me</span>
+                  <span className="text-[14px] whitespace-nowrap">My Trade Me</span>
                 </li>
               </ul>
             </div>
