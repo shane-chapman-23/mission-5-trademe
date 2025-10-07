5fde985305671c6deb3b7601150bd6b5a7198279
Kenneth Sta Maria <kstamaria29@gmail.com>
2025-10-07 15:02:27 +1300

    Refactor: fetch listing data and images from database instead of passing state with useNavigate

--- backend/routes/listingRoutes.js
+++ backend/routes/listingRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {getListings, searchListings} from "../controllers/listingController.js";
+import { getListings, searchListings, getListingById } from "../controllers/listingController.js";
 
 const router = express.Router();
 
 router.get("/", getListings);
 router.get("/search", searchListings);
+router.get("/:id", getListingById);
 
 export default router;
--- frontend/src/sharedComponents/ListingCard.jsx
+++ frontend/src/sharedComponents/ListingCard.jsx
@@ -1,22 +1,18 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import wishlistIcon from "../assets/icons/magnet.svg";
 
-export default function ListingCard({listing, onAddToComparison}) {
+export default function ListingCard({ listing, onAddToComparison }) {
   const navigate = useNavigate();
 
   const openListing = () => {
-    navigate(`/listing/${listing._id}`, {state: {listing}});
+    navigate(`/listing/${listing._id}`);
   };
 
   return (
     <div className="flex flex-col h-[408px] w-[350px] rounded-lg shadow-lg relative ">
       {/* Wishlist button */}
       <button className="absolute top-0 right-0 w-[0] h-0 border-l-[60px] border-l-transparent border-b-[60px] border-b-yellow-400 rotate-270 rounded-b"></button>
-      <img
-        src={wishlistIcon}
-        alt="wishlist icon"
-        className="absolute top-[8px] right-[8px] w-5 h-5"
-      />
+      <img src={wishlistIcon} alt="wishlist icon" className="absolute top-[8px] right-[8px] w-5 h-5" />
       {/* Add to comparison list button */}
       <div className="absolute top-3 left-3">
         <div className="relative flex items-center group">
@@ -43,20 +39,11 @@ export default function ListingCard({listing, onAddToComparison}) {
       </div>
       {/* Image */}
 
-      <div
-        onClick={openListing}
-        className="bg-gray-200 h-[70%] flex justify-center rounded-t-lg cursor-pointer"
-      >
-        <img
-          src={listing.image1}
-          className="w-full object-cover rounded-t-lg"
-        ></img>
+      <div onClick={openListing} className="bg-gray-200 h-[70%] flex justify-center rounded-t-lg cursor-pointer">
+        <img src={listing.image1} className="w-full object-cover rounded-t-lg"></img>
       </div>
       {/* Details */}
-      <div
-        onClick={openListing}
-        className="h-[30%] bg-white cursor-pointer rounded-b-md"
-      >
+      <div onClick={openListing} className="h-[30%] bg-white cursor-pointer rounded-b-md">
         <div className="m-2 h-[100%]">
           <div className="flex justify-between">
             <p className="text-[#646464] text-[8px]">{listing.location}</p>
@@ -64,32 +51,22 @@ export default function ListingCard({listing, onAddToComparison}) {
           </div>
           <div className="flex justify-between my-3">
             <div>
-              <h2 className="text-black text-[12px] font-bold">
-                {listing.title}
-              </h2>
-              <p className="text-gray-500 text-[8px]">
-                ${listing.shipping_cost}.00 shipping nationwide
-              </p>
-              <p className="text-gray-500 text-[8px]">
-                Expected delivery in 2-3 business days
-              </p>
+              <h2 className="text-black text-[12px] font-bold">{listing.title}</h2>
+              <p className="text-gray-500 text-[8px]">${listing.shipping_cost}.00 shipping nationwide</p>
+              <p className="text-gray-500 text-[8px]">Expected delivery in 2-3 business days</p>
             </div>
           </div>
           {/* Current bid */}
           <div className="flex justify-between">
             <div className="h-100% flex flex-col justify-end">
               <p className="text-gray-500 text-[8px]">No reserve</p>
-              <p className="text-black text-[10px]  font-bold">
-                {`$${Number(listing.start_price).toFixed(2)}` || "$10.00"}
-              </p>
+              <p className="text-black text-[10px]  font-bold">{`$${Number(listing.start_price).toFixed(2)}` || "$10.00"}</p>
             </div>
             {/* buy now price */}
             {listing.buy_now && (
               <div className="h-100% flex flex-col justify-end text-right">
                 <p className="text-gray-500 text-[8px]">Buy Now</p>
-                <p className="text-black text-[10px] font-bold">{`$${Number(
-                  listing.buy_now
-                ).toFixed(2)}`}</p>
+                <p className="text-black text-[10px] font-bold">{`$${Number(listing.buy_now).toFixed(2)}`}</p>
               </div>
             )}
           </div>
